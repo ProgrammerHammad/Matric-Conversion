88e7649a620ef1fecd95115e83ad845fa937e487
ShahZaibKhan00 <shahzaib78903@gmail.com>
2023-08-04 03:52:24 +0500

    length

--- Metric EProject/temp.js
+++ Metric EProject/temp.js
@@ -5,6 +5,7 @@ document.getElementById("submit").onclick = convertTemp;
 document.getElementById("reset").onclick = reset;
 
 //picks up on if and when an input field changes, then sets the lastEdited flag to the input field that was changed.
+<<<<<<< Updated upstream
 document.getElementById("celsius").onchange = function() {
   lastEdited = "celsius";
 };
@@ -12,6 +13,15 @@ document.getElementById("fahrenheit").onchange = function() {
   lastEdited = "fahrenheit";
 };
 document.getElementById("kelvin").onchange = function() {
+=======
+document.getElementById("celsius").onchange = function () {
+  lastEdited = "celsius";
+};
+document.getElementById("fahrenheit").onchange = function () {
+  lastEdited = "fahrenheit";
+};
+document.getElementById("kelvin").onchange = function () {
+>>>>>>> Stashed changes
   lastEdited = "kelvin";
 };
 
@@ -39,17 +49,29 @@ function convertTemp(temp) {
     //round the converted fahrenheit and kelvin values
     document.getElementById("fahrenheit").value = Math.round(conversionF);
     document.getElementById("kelvin").value = Math.round(conversionK);
+<<<<<<< Updated upstream
   } 
     //if the fahrenheit field changes, convert the celsius and kelvin values
     else if (lastEdited === "fahrenheit") {
+=======
+  }
+  //if the fahrenheit field changes, convert the celsius and kelvin values
+  else if (lastEdited === "fahrenheit") {
+>>>>>>> Stashed changes
     conversionC = (fahrenheit - 32) * 5 / 9;
     conversionK = conversionC + 273;
     //round the converted celsius and kelvin values
     document.getElementById("celsius").value = Math.round(conversionC);
     document.getElementById("kelvin").value = Math.round(conversionK);
+<<<<<<< Updated upstream
   } 
     //if the kelvin field changes, convert the celsius and fahrenheit values
     else if (lastEdited === "kelvin") {
+=======
+  }
+  //if the kelvin field changes, convert the celsius and fahrenheit values
+  else if (lastEdited === "kelvin") {
+>>>>>>> Stashed changes
     conversionC = kelvin - 273;
     conversionF = conversionC * 9 / 5 + 32;
     //round the converted celsius and fahrenheit values
@@ -69,3 +91,60 @@ function reset() {
   document.getElementById("fahrenheit").value = 0;
   document.getElementById("kelvin").value = 0;
 }
+<<<<<<< Updated upstream
+=======
+
+// Length
+
+let factors1 = new Array(1, 0.001, 0.000621, 3.28084, 1.09361);
+let factors2 = new Array(1000, 1, 0.621, 3280.84, 1093.61);
+let factors3 = new Array(1609.34, 1.60934, 1, 5280, 1760);
+let factors4 = new Array(0.3048, 0.0003048, 0.000189394, 1, 0.333333);
+let factors5 = new Array(0.9144, 0.0009144, 0.000568182, 3, 1);
+let factors = new Array(factors1, factors2, factors3, factors4, factors5);
+
+function convertUnit() {
+  // Grab the correct conversion factor from our arrays
+  fromIndex = document.lengthCon.fromUnit.selectedIndex;
+  toIndex = document.lengthCon.toUnit.selectedIndex;
+  factor = factors[fromIndex][toIndex];
+  // Create the text that we want to write on the middle line
+  fromUnitText = document.lengthCon.fromUnit.options[fromIndex].text;
+  toUnitText = document.lengthCon.toUnit.options[toIndex].text;
+  document.getElementById("formula").innerHTML = fromUnitText + " = " + factor + " " + toUnitText;
+  // Check the input box to make sure we have a valid number
+  if (isNaN(document.lengthCon.fromValue.value))
+    document.getElementById("toValue").innerHTML = "Not a valid number.";
+  // Output the converted number...last number on last line
+  else {
+    document.getElementById("toValue").innerHTML = factor * document.lengthCon.fromValue.value;
+  }
+}
+
+
+// let factors1 = new Array(1, 0.001, 0.000621, 3.28084, 1.09361);
+// let factors2 = new Array(1000, 1, 0.621, 3280.84, 1093.61);
+// let factors3 = new Array(1609.34, 1.60934, 1, 5280, 1760);
+// let factors4 = new Array(0.3048, 0.0003048, 0.000189394, 1, 0.333333);
+// let factors5 = new Array(0.9144, 0.0009144, 0.000568182, 3, 1);
+// let factors = new Array(factors1, factors2, factors3, factors4, factors5);
+
+// function convertUnit() {
+//   // Grab the correct conversion factor from our arrays
+//   fromIndex = document.lengthCon.fromUnit.selectedIndex;
+//   toIndex = document.lengthCon.toUnit.selectedIndex;
+//   factor = factors[fromIndex][toIndex];
+//   // Create the text that we want to write on the middle line
+//   fromUnitText =document.lengthCon.fromUnit.options[document.lengthCon.fromUnit.selectedIndex].text;
+//   toUnitText = document.lengthCon.toUnit.options[document.lengthCon.toUnit.selectedIndex].text;
+//   // console.log("Line 94:", toUnitText);
+//   document.getElementById("formula").innerHTML = fromUnitText + " = " + factor + " " + toUnitText;
+//   // Check the input box to make sure we have a valid number
+//   if (isNaN(document.lengthCon.fromValue.value))
+//     document.getElementById("toValue").innerHTML = "Not a valid number.";
+//   // Output the converted number...last number on last line
+//   else {
+//     document.getElementById("toValue").innerHTML = factor * document.lengthCon.fromValue.value;
+//   }
+// }
+>>>>>>> Stashed changes
